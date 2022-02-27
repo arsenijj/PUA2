2f5a286901d78053446fa9eea8369e29c4fe666b
arsenijj <arsenijjnickitin2001@gmail.com>
2022-02-27 16:05:48 +0400

    Навел красоту, вроде...

--- code/hassevisualization.py
+++ code/hassevisualization.py
@@ -21,7 +21,15 @@ def levels_length(lst):
     return max_level_length_list
 
 
+def get_levels_list(lst, len_levels):
+    levels_list = [[] for _ in range(len(len_levels))]
+    for value in lst:
+        levels_list[value[1] - 1].append(value[0])
+    return levels_list
+
+
 def visual(lst, flag=False):
+
     plt.xlim(-10.0, 10.0)
     lim = lst[-1][1]
     plt.xlabel('Элементы множеств')
@@ -30,66 +38,43 @@ def visual(lst, flag=False):
     if not flag:
         len_levels = levels_length(lst)
 
-        levels_numbers_list = [[] for _ in range(len(len_levels))]
-        for value in lst:
-            levels_numbers_list[value[1] - 1].append(value[0])
+        levels_numbers_list = get_levels_list(lst, len_levels)
+
         plt.ylim(-0.4, 1.2 * lim / 4)
 
-        x = -10
         x_save = -10
-
-        y = -0.3
-        y_save = y
+        y_value = -0.3
+        y_save = y_value
 
         current_level = 0
 
-        delta = 20 / len_levels[current_level]
-        delta1 = delta / 2
-        x += delta1
-
         dy = 0.3
-        dx = 0.2
-        plt.text(x, y, f'{lst[0][0]}')
-        plt.scatter(x + 0.2, y + 0.025, s=250, facecolors='none', edgecolors='black')
-        x += delta
-        for i, value in enumerate(lst[1:]):
-            if lst[i+1][1] == lst[i][1]:
 
-                plt.text(x, y, f'{value[0]}')
-
-                if value[0] > 9:
-                    dx = 0.3
-                if value[0] > 99:
+        for level in levels_numbers_list:
+            x_value = x_save
+            delta = 20 / len_levels[current_level]
+            delta1 = delta / 2
+            x_value += delta1
+            for value in level:
+                plt.text(x_value, y_value, f'{value}')
+                if value > 99:
                     dx = 0.5
-                plt.scatter(x + dx, y + 0.025, s=250, facecolors='none', edgecolors='black')
-
-                dx = 0.2
-                x += delta
-
-            if lst[i][1] != lst[i+1][1]:
-                current_level += 1
-                x = x_save
-                delta = 20 / len_levels[current_level]
-                delta1 = delta / 2
-                x += delta1
-                y += dy
-                if value[0] > 9:
+                elif value > 9:
                     dx = 0.3
-                if value[0] > 99:
-                    dx = 0.5
+                else:
+                    dx = 0.2
+                plt.scatter(x_value + dx, y_value + 0.025, s=250, facecolors='none', edgecolors='black')
+                x_value += delta
+            y_value += dy
+            current_level += 1
 
-                plt.text(x, y, f'{value[0]}')
-                plt.scatter(x + dx, y + 0.025, s=250, facecolors='none', edgecolors='black')
-                dx = 0.2
-                x += delta
-
-        y = y_save + dy
+        y_value = y_save + dy
         for level, values in enumerate(levels_numbers_list[1:]):
             level += 1
-            x = x_save
+            x_value = x_save
             delta = 20 / len_levels[level]
             delta1 = delta / 2
-            x += delta1
+            x_value += delta1
             current_level = level
             for value in values:
                 dividers_lst = dividers(value)
@@ -101,30 +86,29 @@ def visual(lst, flag=False):
                     x_value1 += delta1_value1
                     for value1 in values_levels:
                         if value % value1 == 0 and value1 in dividers_lst:
-                            dividers_lst.remove(value1)
-                            for val in dividers(value1):
-                                if val in dividers_lst:
-                                    dividers_lst.remove(val)
-                            plt.plot([x_value1 + 0.3, x + 0.3], [y - y_previous + 0.08, y - 0.03], color='black')
+                            value1_dividers = dividers(value1)
+                            dividers_lst = [val for val in dividers_lst if val not in value1_dividers + [value1]]
+                            plt.plot([x_value1 + 0.3, x_value + 0.3], [y_value - y_previous + 0.08, y_value - 0.03],
+                                     color='black')
                         x_value1 += delta_value1
                     y_previous += dy
-                x += delta
-            y += dy
+                x_value += delta
+            y_value += dy
     else:
         plt.ylim(-0.4, 2 * lim)
 
-        x = 0
-        y = 0
+        x_value = 0
+        y_value = 0
         dy = 2 * lim / len(lst)
 
-        plt.text(x, y, f'{lst[0][0]}')
-        plt.scatter(x + 0.3, y + 0.1, s=250, facecolors='none', edgecolors='black')
-        y += dy
+        plt.text(x_value, y_value, f'{lst[0][0]}')
+        plt.scatter(x_value + 0.3, y_value + 0.1, s=250, facecolors='none', edgecolors='black')
+        y_value += dy
         for value in lst[1:]:
-            plt.text(x, y, f'{value[0]}')
-            plt.scatter(x + 0.32, y + 0.1, s=250, facecolors='none', edgecolors='black')
-            plt.plot([x + 0.3, x + 0.3], [y - 0.35, y - dy + 0.5], color='black')
-            y += dy
+            plt.text(x_value, y_value, f'{value[0]}')
+            plt.scatter(x_value + 0.32, y_value + 0.1, s=250, facecolors='none', edgecolors='black')
+            plt.plot([x_value + 0.3, x_value + 0.3], [y_value - 0.35, y_value - dy + 0.5], color='black')
+            y_value += dy
 
     plt.show()
 
